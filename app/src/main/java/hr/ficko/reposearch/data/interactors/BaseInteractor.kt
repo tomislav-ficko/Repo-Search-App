d6ca1162e8530f02a337fe9ee01c1d545d177a83
tomislav-ficko <ficko.tomislav@gmail.com>
2020-11-24 19:55:53 +0100

    Fix wrong return type

--- app/src/main/java/hr/ficko/reposearch/data/interactors/BaseInteractor.kt
+++ app/src/main/java/hr/ficko/reposearch/data/interactors/BaseInteractor.kt
@@ -1,11 +1,9 @@
 package hr.ficko.reposearch.data.interactors
 
 import androidx.annotation.WorkerThread
-import androidx.lifecycle.LiveData
-import hr.ficko.reposearch.data.models.RepositoryResponseModel
 
 interface BaseInteractor<in RequestType, out ResponseType> {
 
     @WorkerThread
-    fun execute(params: RequestType): LiveData<RepositoryResponseModel>
+    fun execute(params: RequestType): ResponseType
 }
--- app/src/main/java/hr/ficko/reposearch/data/interactors/Interactors.kt
+++ app/src/main/java/hr/ficko/reposearch/data/interactors/Interactors.kt
@@ -1,8 +1,10 @@
 package hr.ficko.reposearch.data.interactors
 
+import androidx.lifecycle.LiveData
 import hr.ficko.reposearch.data.models.RepositoryRequestModel
 import hr.ficko.reposearch.data.models.RepositoryResponseModel
 
 interface Interactors {
-    interface RepositoryInteractor : BaseInteractor<RepositoryRequestModel, RepositoryResponseModel>
+    interface RepositoryInteractor :
+        BaseInteractor<RepositoryRequestModel, LiveData<RepositoryResponseModel>>
 }
