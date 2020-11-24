681dceec3b6c49b3c89ddaf6d3c1f4ceb1fc9794
tomislav-ficko <ficko.tomislav@gmail.com>
2020-11-24 17:37:41 +0100

    Add method for retrieving the ApiService

--- app/src/main/java/hr/ficko/reposearch/data/MainRepository.kt
+++ app/src/main/java/hr/ficko/reposearch/data/MainRepository.kt
@@ -1,4 +1,24 @@
 package hr.ficko.reposearch.data
 
+import com.squareup.moshi.Moshi
+import hr.ficko.reposearch.data.network.ApiService
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
 class MainRepository {
+
+    fun getApiService(): ApiService {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.github.com")
+            .addConverterFactory(MoshiConverterFactory.create(moshi()))
+            .client(okHttpClient())
+            .build()
+
+        return retrofit.create(ApiService::class.java)
+    }
+
+    fun okHttpClient(): OkHttpClient = OkHttpClient.Builder().build()
+
+    fun moshi(): Moshi = Moshi.Builder().build()
 }
