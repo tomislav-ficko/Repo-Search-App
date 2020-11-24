e9da389ea62fc501cced3a09c3830b7799ca37fc
tomislav-ficko <ficko.tomislav@gmail.com>
2020-11-24 17:31:00 +0100

    Add ViewModel

---
+++ app/src/main/java/hr/ficko/reposearch/ui/MainViewModel.kt
@@ -0,0 +1,11 @@
+package hr.ficko.reposearch.ui
+
+import androidx.lifecycle.ViewModel
+import hr.ficko.reposearch.data.MainRepository
+
+class MainViewModel constructor(
+    val mainRepository: MainRepository
+) : ViewModel() {
+
+
+}
