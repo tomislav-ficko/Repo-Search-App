af030530d04df554a937aaa84f6e2b8776816d8a
tomislav-ficko <ficko.tomislav@gmail.com>
2020-11-24 20:01:52 +0100

    Fix dependencies

--- app/src/main/java/hr/ficko/reposearch/ui/MainActivityViewModel.kt
+++ app/src/main/java/hr/ficko/reposearch/ui/MainActivityViewModel.kt
@@ -1,12 +1,11 @@
 package hr.ficko.reposearch.ui
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
 import hr.ficko.reposearch.data.MainRepository
-import hr.ficko.reposearch.data.interactors.Interactors
 import hr.ficko.reposearch.data.interactors.RepositoryInteractor
 import hr.ficko.reposearch.data.models.RepositoryRequestModel
 import hr.ficko.reposearch.data.models.RepositoryResponseModel
+import javax.inject.Inject
 
 class MainActivityViewModel @Inject constructor(
     val mainRepository: MainRepository,
